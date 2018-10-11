c4cdb613787e32abf75a43903361284d545cf52f
Bayu Fauzi Saputra <43790981+bayzi3006@users.noreply.github.com>
2018-10-12 00:31:43 +0700

    Add files via upload

---
+++ tugas01.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    //Deklarasi Variable
+    int Bilangan;
+   
+    //Input Tampilan
+    cout<<"Masukkan Bilangan:";cin>>Bilangan;
+    cout<<endl;
+   
+string message;
+message=Bilangan%2==0?"Bilangan Genap":"Bilangan Ganjil";
+cout<<message;
+
+return 0;
+}
